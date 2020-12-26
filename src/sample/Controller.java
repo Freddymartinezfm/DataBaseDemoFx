0220ef66255c007b4fbee3501ff7ae80869caaa5
Freddy Martinez <freddymartinezfm@icloud.com>
2020-12-26 15:52:58 -0600

    added cellValueFactory to TableColumn fxml

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,24 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import sample.model.Artist;
+import sample.model.Datasource;
+
 public class Controller {
+}
 
 
 
 
+class GetAllArtistsTask extends Task {
 
+	@Override
+	public ObservableList<Artist> call() {
 
-
+		return FXCollections.observableArrayList(
+				Datasource.getInstance().queryArtists(Datasource.ORDER_BY_ASC));
+		}
 
 }
